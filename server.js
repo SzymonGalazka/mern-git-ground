b3d070d2ce0e81f6f16ad6b6e3766d4f0ce7503b
Szymon Gałązka <32265060+SzymonGalazka@users.noreply.github.com>
2020-03-15 11:30:26 +0100

    add profile and user models, middleware authentication

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db.js');
+
+const users = require('./routes/api/users.js');
+const posts = require('./routes/api/posts.js');
+const auth = require('./routes/api/auth.js');
+const profile = require('./routes/api/profile.js');
 
 const app = express();
 
+//Connect DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/',(req, res) => {res.send('API Running')});
+
+//Define Routes
+app.use('/api/users',users);
+app.use('/api/posts',posts);
+app.use('/api/auth',auth);
+app.use('/api/profile',profile);
 
 const PORT = process.env.PORT || 5000;
 
