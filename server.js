d5fa55ffa96e34518c80153124ae5ec1c8256c09
Szymon Gałązka <32265060+SzymonGalazka@users.noreply.github.com>
2020-03-17 15:45:54 +0100

    add profile handling

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const connectDB = require('./config/db.js');
+const express = require("express");
+const connectDB = require("./config/db.js");
 
-const users = require('./routes/api/users.js');
-const posts = require('./routes/api/posts.js');
-const auth = require('./routes/api/auth.js');
-const profile = require('./routes/api/profile.js');
+const users = require("./routes/api/users.js");
+const posts = require("./routes/api/posts.js");
+const auth = require("./routes/api/auth.js");
+const profile = require("./routes/api/profile.js");
 
 const app = express();
 
@@ -12,15 +12,17 @@ const app = express();
 connectDB();
 
 //Init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
-app.get('/',(req, res) => {res.send('API Running')});
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
 
 //Define Routes
-app.use('/api/users',users);
-app.use('/api/posts',posts);
-app.use('/api/auth',auth);
-app.use('/api/profile',profile);
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/auth", auth);
+app.use("/api/profile", profile);
 
 const PORT = process.env.PORT || 5000;
 
