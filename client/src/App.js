1c8444c00a055fa63a3a832925e8e3aa4a5254d5
Szymon Gałązka <32265060+SzymonGalazka@users.noreply.github.com>
2020-03-20 14:48:02 +0100

    basic redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import "./App.css";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const iconList = Object.keys(Icons)
   .filter(key => key !== "fas" && key !== "prefix")
@@ -16,18 +19,20 @@ const iconList = Object.keys(Icons)
 library.add(...iconList);
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
