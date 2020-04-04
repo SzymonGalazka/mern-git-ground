c3d0455b83ab6488d3bc592d91d9b06725c0d825
Szymon Gałązka <32265060+SzymonGalazka@users.noreply.github.com>
2020-04-04 23:53:01 +0200

    add heroku config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db.js");
+const path = require("path");
 
 const users = require("./routes/api/users.js");
 const posts = require("./routes/api/posts.js");
@@ -14,16 +15,21 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 //Define Routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 
+// Serve static assets for production
+if (process.env.NODE_ENV == "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
